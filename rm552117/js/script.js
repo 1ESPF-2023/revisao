c5fd8fcf441b1ae9b491c5c2df7f3c8be348689d
MuriloEloys <rm552117@fiap.com.br>
2023-08-07 09:52:43 -0300

    finalização da aula

--- rm552117/js/script.js
+++ rm552117/js/script.js
@@ -28,3 +28,16 @@ nomesAlunosArray.forEach((aluno) =>  {
     console.log(`${aluno} ` );  
 });
 
+//Recuperando a div lista-nomes e criando alguns filhos nela.
+const divListaNomes = document.querySelector("#lista-nome")
+
+//Criar os elementos filhos da div que são 'p'
+let p = document.createElement('p');
+
+nomesAlunosArray.forEach((aluno)=>{
+    let p=document.createElement('p');
+    p.textContent=aluno;
+    p.setAttribute('class','nomes');
+    divListaNomes.appendChild(p);
+
+})
