779f2192dd0cfdecaae5371b208ad877aa07a14e
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-08-07 09:23:47 -0300

    Declarando nomes de alunos!

---
+++ rm552047/js/script.js
@@ -0,0 +1,6 @@
+// Criar uma lista de nomes de alunos com um array
+
+let nomesAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Jefferson","Rony"]
+
+//Imprimindo o Array de nomes
+console.log(nomesAlunosArray)
