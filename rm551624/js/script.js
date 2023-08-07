5541c80177f14effb9aa5091d2f7daa284dbad32
ThiagoJO0705 <rm551624@fiap.com.br>
2023-08-07 09:24:14 -0300

    Primeiro array de nome de alunos

---
+++ rm551624/js/script.js
@@ -0,0 +1,5 @@
+// Criar uma lista de nomes de alunos em um array
+let nomesAlunosArray = ["Pedro", "Luis", "Carlos", "Maria", "Joice", "Otávio", "Antônio", "Jefferson", "Rony"];
+//Imprimindo o Array de nomes
+console.log(nomesAlunosArray)
+
