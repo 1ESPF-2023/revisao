6e1f33f87f591bc674742163f835e5879ab23258
Migs123 <rm551239@fiap.com.br>
2023-08-07 09:54:57 -0300

    teminando revisao 1/?

--- rm551239/js/script.js
+++ rm551239/js/script.js
@@ -1,21 +1,13 @@
 // Criar uma lista de nomes em um array
 let nomesAlunosArray = ['Luiz','Marcelo','Osvaldo','Mariele','Izaquias','Josoé','Simone','Roni','Sonia','Sophia'];
-//Quebrando a lina
-console.log("\n");
-
 //Imprimindo o Array
 console.log(nomesAlunosArray);
-//Quebrando a lina
-console.log("\n");
-
 //Acessando a primeira posição do array de nomes
-console.log("Primeiro Aluno: " + nomesAlunosArray[0]);
-//Quebrando a lina
-console.log("\n");
+// console.log("1° Aluno: " + nomesAlunosArray[0]);
 
 //Acesso aos elementos da segunda à quarta posição
-for (var i=1; i<=3; i++) {
-    console.log(`Segundo Aluno ${i}: ${nomesAlunosArray[i]}` );
+for (var i=0; i<=9; i++) {
+    console.log(`${i+1}° Aluno: ${nomesAlunosArray[i]}` );
 };
 //Quebrando a lina
 console.log("\n");
@@ -25,3 +17,17 @@ nomesAlunosArray.forEach((aluno)=> {
     //Imprimndo cada elemento no console
     console.log(`${aluno}`);
 });
+
+//recuperando a div lista-nomes e criando childs nela
+const divListaNome = document.querySelector("#lista-nome");
+
+nomesAlunosArray.forEach((aluno)=>{
+    //criar os elementos child da div que são "p";
+    let p = document.createElement("p");
+    p.innerText = aluno;
+
+    p.setAttribute("class","nomes");
+
+    divListaNome.appendChild(p);
+});
+
