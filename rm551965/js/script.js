3ed68fa3690e8ec17c0bb016f941e8d8911e407e
NicolasBoni <rm551965@fiap.com.br>
2023-08-07 09:25:10 -0300

    base do projeto criada

---
+++ rm551965/js/script.js
@@ -0,0 +1,6 @@
+
+
+//Criar uma lista de nomes de alunos em um array
+let nomesAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Jefferson","Rony"];
+//Imprimindo o Array de nomes
+console.log(nomesAlunosArray);
