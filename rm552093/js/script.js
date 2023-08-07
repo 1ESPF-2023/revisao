7fc785b91e02504a40c2ee960f0042f24eba74e0
medeirosg601 <rm552093@gmail.com>
2023-08-07 09:52:25 -0300

    aula 07/08

--- rm552093/js/script.js
+++ rm552093/js/script.js
@@ -1,3 +1,17 @@
-let nomesAlunosArray = ["Pedro", "Carlos", "Otavio", "Joice"]
+let nomesAlunosArray = ["Pedro", "Carlos", "Otavio", "Joice"];
 
-console.log(nomesAlunosArray)
+console.log(nomesAlunosArray);
+
+console.log("/n");
+
+console.log("Primeiro Aluno: " + nomesAlunosArray[0]);
+
+for (var i = 1; 1<=3 ; i++) {
+    console.log(`Segundo aluno ${i}: ${nomesAlunosArray[i]}`)
+};
+
+console.log("/n");
+
+nomesAlunosArray.forEach((aluno) => {
+    console.log(`${aluno}`)
+});
