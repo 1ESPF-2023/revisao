ae4da34f663a7cf9f900d93cbcf3b76960c7659b
ThiagoJO0705 <rm551624@fiap.com.br>
2023-08-07 09:52:47 -0300

    Imprimindo os nomes do array no html

--- rm551624/js/script.js
+++ rm551624/js/script.js
@@ -17,6 +17,23 @@ console.log("\n");
 //Imprimindo todos os alunos com forEach
 nomesAlunosArray.forEach((aluno) => {
     //Imprime cada elemento no console
-    console.log(`${aluno}`)
+    console.log(`${aluno}`);
 })
+
+//Reccuperando a div lista-nomes e criando alguns filhos nela.
+const divListaNomes = document.querySelector("#lista-nome");
+
+
+nomesAlunosArray.forEach((aluno) => {
+    //Criar os elementos filhos da div que são 'p'
+    //cria vários elementos no html 'p'
+    let p = document.createElement("p");
+
+    //Escreve no html os nomes da lista
+    p.textContent = aluno;
+
+    p.setAttribute("class", "nomes");
+    divListaNomes.appendChild(p);
+})
+
 
