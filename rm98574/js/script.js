3dd236f0e0f086569a23d336ee8074f1400ccbeb
DriLopess <rm98574@fiap.com.br>
2023-08-07 09:55:54 -0300

    Revisão

---
+++ rm98574/js/script.js
@@ -0,0 +1,34 @@
+//Criar uma lista de alunos em um array
+let nomesAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Filipe","Jailson","Rony"]
+//Imprimindo o Array de nomes
+console.log(nomesAlunosArray);
+//Quebrando linha
+console.log("\n");
+
+//Acessar a primeira posição do array de nomes de alunos.
+console.log("Primeiro Aluno: " + nomesAlunosArray[0]);
+
+//Acessando os elementos da segunda até a quarta posições dos arrays
+for (var i=1; i<=3; i++){
+    console.log(`Segundo Aluno ${i}: ${nomesAlunosArray[i]}`);
+};
+
+//Imprimindo todos os alunos com forEach
+nomesAlunosArray.forEach((aluno) => {//Imprime cada elemento no console
+   console.log(`${aluno}`);});
+
+//Recuperando a div lista-nomes e criando alguns filhos nela.
+const divListaNomes = document.querySelector("#lista-nome");
+
+//Criar os elementos filhos da div que são 'p';
+let p = document.createElement("p");
+
+nomesAlunosArray.forEach(aluno)=>{
+    p.textContent = aluno;
+
+
+    
+    divListaNomes.appendChild(p);
+});
+
+
