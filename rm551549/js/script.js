d70f27502d63f71e94d51ab0faafb25386634fb9
Rony ken <rm551549@fiap.com.br>
2023-08-07 09:23:20 -0300

    Declarando array de nomes de Alunos

---
+++ rm551549/js/script.js
@@ -0,0 +1,6 @@
+//Criar uma lista de nomes de alunos em um array
+let nomesAlunosArray = ["Pedro", "Luis","Carlos","Maria","Joice","Ótavio","Antônio","Jefferson","Rony"];
+
+//Imprimi o Array
+
+console.log(nomesAlunosArray);
