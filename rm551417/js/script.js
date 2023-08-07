e0e44d844ca27a98cfe40fa4e71ca09285bf9a47
tomazcarballo <rm551417@fiap.com.br>
2023-08-07 09:31:27 -0300

    Imprimindo com for e forEach.

--- rm551417/js/script.js
+++ rm551417/js/script.js
@@ -1,3 +1,20 @@
-let nomeAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Jefferson","Rony"];
+let nomesAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Jefferson","Rony"];
 
-console.log(nomeAlunosArray);
+console.log(nomesAlunosArray);
+
+console.log("\n");
+
+//Acessar a primeira posição do array de nomes de alunos.
+console.log("Aluno 0: " + nomesAlunosArray[0]);
+
+console.log("\n");
+
+for (var i=1; i<=3 ; i++) {
+    console.log(`Aluno ${i}: ${nomesAlunosArray[i]}`);
+};
+
+console.log("\n");
+
+nomesAlunosArray.forEach((aluno)=>{
+    console.log(`${aluno}`);
+});
