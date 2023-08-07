343cc87100c8134c628192ed33a149556ba8dea5
G3n4r00 <rm551986@fiap.com.br>
2023-08-07 09:23:02 -0300

    delcarando array de nomes

---
+++ rm551986/js/script.js
@@ -0,0 +1,5 @@
+//criar uma lista de nomes de alunos em um array 
+
+let nomesAlunosArray = ["Pedro","Luis","Carlos", "Maria","Joice","Otávio","Antônio","Jefferson","Rony"];
+//Imprimindo o array
+console.log(nomesAlunosArray)
