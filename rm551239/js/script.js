ddfcb1239c25627c8cdbc106522dd6b9983b1dcb
Migs123 <rm551239@fiap.com.br>
2023-08-07 09:23:13 -0300

    Declarando Array de nomes

---
+++ rm551239/js/script.js
@@ -0,0 +1,6 @@
+// Criar uma lista de nomes em um array
+let nomesAlunosArray = ['Luiz','Marcelo','Osvaldo','Mariele','Izaquias','Josoé','Simone','Roni','Sonia','Sophia']
+//Imprimindo o Array
+console.log(nomesAlunosArray)
+
+
