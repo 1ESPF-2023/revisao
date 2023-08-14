e1166d0c16217473999637ecf517da05803a9248
Migs123 <rm551239@fiap.com.br>
2023-08-14 08:40:36 -0300

    exs array

---
+++ rm551239/js/tarefa.js
@@ -0,0 +1,14 @@
+let listaTarefasArray = [];
+
+//Recuperando o botão com querySelector
+const botaoAddTarefa = document.querySelector("#btnAddTarefa");
+
+//Adiciona evento de click no botão para adicionar tarefa ao array e renderizar a view.
+botaoAddTarefa.addEventListener("click", (evento)=>{
+    
+    // evento.preventDefault();
+
+    
+
+    console.log(evento.target);
+})
