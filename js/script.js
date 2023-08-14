fc0fc0498591ac509f5c91296600a37f49ba3737
ricardoVergani <rm550166@fiap.com>
2023-08-14 10:08:47 -0300

    criacao de branch e atualizacao de conteudo

---
+++ js/script.js
@@ -0,0 +1,61 @@
+//Criar uma lista de nomes de alunos em um array
+let nomesAlunosArray = ["Pedro","Luis","Carlos","Maria","Joice","Otávio","Antônio","Jefferson","Rony"];
+//Imprimindo o Array de nomes
+console.log(nomesAlunosArray);
+
+//Quebrando linha
+console.log("\n");
+
+//Acessar a primeira posição do array de nomes de alunos.
+console.log("Primeiro Aluno: " + nomesAlunosArray[0]);
+
+//Quebrando linha
+console.log("\n");
+
+//Acessando os elementos da segunda até a quarta posições dos arrays
+for (var i=1; i<=3 ; i++) {
+    console.log(`Segundo Aluno ${i}: ${nomesAlunosArray[i]}`);
+};
+
+//Quebrando linha
+console.log("\n");
+
+//Imprimindo todos os alunos com forEach
+nomesAlunosArray.forEach((aluno) =>  {
+    //Imprime cada elemento no console
+    console.log(`${aluno} ` );  
+});
+
+//Recuperando a div lista-nomes e criando alguns filhos nela.
+const divListaNomes = document.querySelector("#lista-nome");
+
+//Criar os elementos filhos da div que são 'p';
+
+// nomesAlunosArray.forEach((aluno)=>{
+//     let p = document.createElement("p");
+//     p.innerText = aluno;
+
+//     p.setAttribute("class","nomes");
+    
+//     divListaNomes.appendChild(p);
+// });
+
+
+let novoArrayComIndicesPares = nomesAlunosArray.map((aluno,indice)=>{
+    let p = document.createElement("p");
+    p.innerText = (indice+1)+" - "+aluno;
+
+    p.setAttribute("class","nomes");
+    
+    divListaNomes.appendChild(p);
+
+    if((indice+1) %2 == 0){
+        return aluno;
+    }
+    
+
+}).filter(aluno =>{
+    return aluno!= undefined
+});
+
+console.log(novoArrayComIndicesPares );
